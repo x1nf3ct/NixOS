f377c372827ab52b771e9ac62428634c1f450054
x1nf3ct <mostafa.cybersec@gmail.com>
2025-08-10 13:30:37 +0300

    Update account.nix

--- system/modules/account.nix
+++ system/modules/account.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  users.users.ab0z3in4 = {
+  users.users.x1nf3ct = {
     isNormalUser = true;
     initialPassword = "password";
     extraGroups = [ "wheel" "networkmanager" "audio" "video" "libvirtd" ];
@@ -12,9 +12,9 @@
     interactiveShellInit = ''eval "$(starship init bash)"'';
     shellAliases = {
       update-repo = "sudo nix flake update --flake /etc/nixos";
-      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#ab0z3in4-PC";
-      upgrade = "sudo nixos-rebuild switch --flake /etc/nixos#ab0z3in4-PC --upgrade";
-      rebuild-boot = "sudo nixos-rebuild boot --flake /etc/nixos#ab0z3in4-PC";
+      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#x1nf3ct-PC";
+      upgrade = "sudo nixos-rebuild switch --flake /etc/nixos#x1nf3ct-PC --upgrade";
+      rebuild-boot = "sudo nixos-rebuild boot --flake /etc/nixos#x1nf3ct-PC";
       remove-garbage = "sudo nix-collect-garbage -d";
     };
   };
