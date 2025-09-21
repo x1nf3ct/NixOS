e8fe5e0679863cc09b3bda8db456fc011b867179
x1nf3ct <mostafa.cybersec@gmail.com>
2025-09-21 12:58:33 +0300

    Update fonts.nix

--- home-manager/config/fonts.nix
+++ home-manager/config/fonts.nix
@@ -2,9 +2,9 @@
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
-      serif = [ "IBM Plex Serif" "IBM Plex Sans Arabic" "Noto Serif CJK" ];
-      sansSerif = [ "Fira Sans" "IBM Plex Sans Arabic" "Noto Sans CJK" ];
-      monospace = [ "JetBrains Mono" ];
+      serif = [ "IBM Plex Serif" "IBM Plex Sans Arabic" "Noto Serif" "Noto Serif CJK" ];
+      sansSerif = [ "Fira Sans" "IBM Plex Sans Arabic" "Noto Sans" "Noto Sans CJK" ];
+      monospace = [ "JetBrains Mono" "Noto Sans Mono" ];
       emoji = [ "Noto Color Emoji" "Twemoji" "Segoe UI Emoji" "Apple Color Emoji" ];
     };
   };
