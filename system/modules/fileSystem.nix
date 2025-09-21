8d4031f3c4d180b013a3419111974f6ea6fe162c
x1nf3ct <mostafa.cybersec@gmail.com>
2025-09-21 12:37:51 +0300

    Update fileSystem.nix

--- system/modules/fileSystem.nix
+++ system/modules/fileSystem.nix
@@ -4,7 +4,7 @@
     "/home".options = [ "compress=zstd" ];
     "/nix".options = [ "compress=zstd" "noatime" ];
     "/mnt/Data" = {
-      device = "/dev/disk/by-uuid/49ae99c2-2d28-4758-9af3-ac93ddcbba0c";
+      device = "/dev/disk/by-uuid/7eff8774-4ac1-48cb-adab-e72b19396083";
       fsType = "ext4";
       options = [ "defaults" "noatime" "barrier=1" "data=ordered" "errors=remount-ro" "commit=300" "nofail" ];
     };
